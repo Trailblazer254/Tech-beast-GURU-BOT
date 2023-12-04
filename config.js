e27b228003cccc9d55a711c999cb762a5a62a3eb
Trailblazer254 <135078432+Trailblazer254@users.noreply.github.com>
2023-12-05 00:00:54 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['0750678057', 'me', true],
+  ['0750678057', 'Trailtechbeast', true], 
   [''] 
 ] //Number of owners
 
